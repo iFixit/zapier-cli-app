642465aba60bfbd4cc9d328a23088b3ce19ccde3
hackalot805 <shaun@dozuki.com>
2017-12-12 17:26:30 -0800

    use api class

--- creates/createDataCaptureInWorkLogEntry.js
+++ creates/createDataCaptureInWorkLogEntry.js
@@ -1,3 +1,27 @@
+const dozukiAPI = require('../tools/dozukiAPI');
+
+const extractDataFromResponse = (response, bundle) => {
+   // The API does not return a response... not sure if this is a bug.
+   // TODO: Find out if this is a bug!
+   return {};
+};
+
+const createDataCaptureInWorkLogEntry = (z, bundle) => {
+   let dAPI = new dozukiAPI(bundle.authData.siteName);
+
+   dAPI.timeout  = 3000; // Creates seem to take a little longer?
+   dAPI.endpoint = ['work_log', bundle.inputData.entryId, 'stepData'];
+   dAPI.callback = extractDataFromResponse;
+
+   dAPI.body.fields =  bundle.inputData.fields;
+
+   if (bundle.inputData.lastTimingId) {
+      dAPI.body.lasttimingid = bundle.inputData.lastTimingId;
+   }
+
+   return dAPI.putDataOnEndpoint(z, bundle);
+};
+
 module.exports = {
    key: 'createDataCaptureInWorkLogEntry',
    noun: 'comment',
@@ -13,23 +37,9 @@ module.exports = {
             helpText: "List of values keyed on field_revisionid from the Step Form. In the example below, the field with field_revisionid '123' has received input from the user of string 'example', and the field with field_revisionid '124' has received input of a checked checkbox."},
          {key: 'lastTimingId', required: false, type: 'string', helpText: "The most recent timingid from a previous request. This prevents multiple sessions from getting out of sync."},
       ],
-      perform: (z, bundle) => {
-         let myBody = {
-            fields: bundle.inputData.fields
-         };
-         const promise = z.request({
-            url: 'https://' + bundle.authData.siteName + '.dozuki.com/api/2.0/work_log/' + bundle.inputData.entryId + '/stepData',
-            method: 'PUT',
-            body: JSON.stringify(myBody),
-            headers: {
-               'content-type': 'application/json'
-            }
-         });
-
-         // The API does not return a response... not sure if this is a bug.
-         // TODO: Find out if this is a bug!
-         return promise.then((response) => JSON.parse("{}"));
-      },
+      perform: createDataCaptureInWorkLogEntry,
+// The API does not return a response... not sure if this is a bug.
+// TODO: Find out if this is a bug!
 //      sample: {},
 //      outputFields: []
    }
