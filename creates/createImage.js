efd84c0c05379f2021fedaadd6cc02fac78f2a97
hackalot805 <shaun@dozuki.com>
2017-12-14 15:47:36 -0800

    Use 'file' type inputData.

--- creates/createImage.js
+++ creates/createImage.js
@@ -5,11 +5,17 @@ const createImage = (z, bundle) => {
 
    dAPI.timeout   = 5000; // Image upload can take a while?
    dAPI.endpoint  = ['user', 'media', 'images'];
+
+   dAPI.getParams.file = bundle.inputData.file;
+   dAPI.getParams.cropToRatio = bundle.inputData.cropToRatio;
+   dAPI.body = bundle.inputData.fileData;
+
+   console.log(dAPI.getParams);
 //   dAPI.callback  = extractDataFromResponse;
-   dAPI.body.file = encodeURI(bundle.inputData.file);
-   if (bundle.inputData.cropToRatio) {
-      dAPI.body.cropToRatio = bundle.inputData.cropToRatio;
-   }
+//   dAPI.body.file = encodeURI(bundle.inputData.file);
+//   if (bundle.inputData.cropToRatio) {
+//      dAPI.body.cropToRatio = bundle.inputData.cropToRatio;
+//   }
 
    return dAPI.postDataOnEndpoint(z, bundle);
 };
@@ -23,9 +29,10 @@ module.exports = {
    },
    operation: {
       inputFields: [
-         {key: 'file', required: true, type: 'string', label: 'File',
-            helpText: "Filename of the image being uploaded."},
-         {key: 'cropToRatio', required: false, type: 'string', label: 'Crop To Ratio'
+         {key: 'file', required: true, type: 'file', label: 'File',
+            helpText: "File to be used.  Can be uploaded now, or you can use the result of a trigger."},
+         {key: 'cropToRatio', required: true, label: 'Crop To Ratio'
+            , choices: {ONE_ONE:'One One', FOUR_THREE:'Four Three', VARIABLE: 'Variable'}
             , helpText: 'If necessary, crops the image to the specified ratio.'},
       ],
       perform: createImage,
